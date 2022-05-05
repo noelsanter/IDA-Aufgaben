0738afe6eadf104f0f477cb3e472bb6091675f8d
noelsanter <noel.santer@stud.hslu.ch>
2022-05-05 15:58:51 +0200

    gg

--- Exercise/TAG11/assets/js/script.js
+++ Exercise/TAG11/assets/js/script.js
@@ -16,10 +16,14 @@ function neuesElement() {
     document.body.appendChild(div);
 }
 
-function neuesElement() {
-    let div = document.createElement("div");
-    div.classList.add('random');
-    div.style.left = Math.random() * window.innerWidth + "px";
-    div.style.top = Math.random() * window.innerHeight + "px";
-    document.body.appendChild(div);
+
+function neuesBild() {
+    n++;
+    let versatz = 20;
+    let myImage = new Image(200, 100);
+    myImage.src = 'assets/images/stalking.jpg';
+    myImage.classList.add('bild');
+    myImage.style.left = Math.random() * window.innerWidth + "px";
+    myImage.style.top = Math.random() * window.innerHeight + "px";
+    document.body.appendChild(myImage);
 }
