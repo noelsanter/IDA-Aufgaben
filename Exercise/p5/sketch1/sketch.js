5ab658fcc89ac690d526349fb2863053546e1277
noelsanter <noel.santer@stud.hslu.ch>
2022-05-10 11:25:00 +0200

    dd

--- Exercise/p5/sketch1/sketch.js
+++ Exercise/p5/sketch1/sketch.js
@@ -1,11 +1,11 @@
 function setup() {
     // put setup code here
-    createCanvas(640, 640);
+    createCanvas(1300, 700);
     background(255);
 }
 
 function draw() {
     // put drawing code here
     background(255, 50);
-    ellipse(mouseX, mouseY, 44, 88);
+    rect(mouseX, mouseY, 44, 88, 20);
 }
