0cd7e581507b3831b5080c5369e79301943214ee
noelsanter <noel.santer@stud.hslu.ch>
2022-05-10 11:13:52 +0200

    hh

--- Exercise/p5/sketch2/sketch.js
+++ Exercise/p5/sketch2/sketch.js
@@ -1,20 +1,32 @@
 let history;
+let count = 0;
 
 function setup() {
     // put setup code here
-    createCanvas(640, 640);
+    createCanvas(window.innerWidth, window.innerHeight);
     let url = "chrome_history.json";
     loadJSON(url, loaded);
     background(255);
+    textSize(60);
+    textAlign(CENTER, CENTER);
+    frameRate(1);
 }
 
 function draw() {
-    // put drawing code here
     background(255, 50);
-    rect(30, 20, 55, 55, 20);
+    //rect(30, 20, 55, 55, 20);
+    let textGroesse = map(history[count].visitCount, 1, 20, 14, 36);
+    textSize(textGroesse);
+
+    text(history[count].title, width / 2, height / 2);
+    count = count + 1;
 }
 
 function loaded(data) {
     history = data;
     console.log(history[0].title);
+
+    let aktuellestunde = 9;
+    let deckungsgrad = map(aktuellestunde, 0, 23, 0, 255)
+    console.log(deckungsgrad)
 }
